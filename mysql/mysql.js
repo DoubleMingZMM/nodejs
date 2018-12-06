79be068bd6b4e9a5fce0cf5a98a8ffd534d154f2
Daniel <1602268076@qq.com>
2018-12-06 13:36:05 +0800

    update

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -7,14 +7,31 @@ const connection = mysql.createConnection({
   database : 'chatRoom'
 })
 
+let result = ''
+
+const sql = 'insert into USER values (null, "明明", "男", ?)'
+
+const sqlParams = [20]
+
+// 这些都是异步函数
 connection.connect(function(err) {
   console.log('我连上Mysql了================>')
 })
 
 connection.query('select * from USER',function(err, rows) {
-  console.log("rows=========================>", rows)
+  result = rows
+  printResult()
+})
+
+connection.query(sql,sqlParams,function(err, rows) {
+  result = rows
+  printResult()
 })
 
 connection.end(function(err) {
   console.log('我退出MYsql了================>')
 })
+
+function printResult() {
+  console.log("result==================>", result)
+}
