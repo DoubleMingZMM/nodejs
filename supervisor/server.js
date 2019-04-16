4fe2d2fa06873547752953ebdcab97e2ce471c81
Daniel <1602268076@qq.com>
2019-04-16 12:45:24 +0800

    update

--- supervisor/server.js
+++ supervisor/server.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 http.createServer(function (req,res) {
     res.writeHead(200,{'Content-Type': 'text/plain'})
-    res.end('hello boy!!!!')
+    res.end('hello boy!!!')
 }).listen(9999,"localhost")
 
 console.log('守护进程已经开启')
