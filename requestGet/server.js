1dfad6204a1fd6d5c1789821ca27df6570d8cbd1
Daniel <1602268076@qq.com>
2019-04-16 12:18:34 +0800

    update

--- requestGet/server.js
+++ requestGet/server.js
@@ -1,6 +1,8 @@
 var http = require('http')
 
 http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end('Hello World\n' + req.method)
+  res.writeHead(200, {"Content-Type": "application/json"})
+  res.end('Hello World boy!!\n' + req.method)
 }).listen(9999, "localhost")
+
+console.log("服务端程序已就绪。。。")
